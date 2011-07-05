05c9a75d7fbb1a3aa493ac65d74792b833917f24
Amador Pahim <amador@pahim.org>
2011-07-05 17:21:55 -0300

    Scirpt para criação/remoção de usuários no Linux

---
+++ LinuxUsers.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+Add(){
+
+while getopts ":b:h:p:u:" Opt
+do
+	case $Opt in
+                b)
+                        Bash="$OPTARG"
+                ;;
+                h)
+                        Home="$OPTARG"
+                ;;
+                p)
+                        Password="$OPTARG"
+                ;;
+		u)
+			User="$OPTARG"
+		;;
+		\?)
+			echo "Opção inválida: -$OPTARG"
+			exit 1
+		;;
+		:)
+			echo "Opção -$OPTARG necessita argumento."
+			exit 1
+		;;
+	esac
+done
+
+while [ ! $Bash ] 
+do
+	read -p "Informe o shell: " Bash
+done
+
+while [ ! $Home ] 
+do
+	read -p "Informe o home: " Home
+done
+
+while [ ! $Password ] 
+do
+	read -s -p "Informe a senha: " Password
+done
+
+while [ ! $User ] 
+do
+	read -p "Informe o usuario: " User
+done
+
+useradd -s "$Bash" -d "$Home" "$User" && {
+	echo "Usuário $User criado com sucesso"
+	echo "$Password" | passwd --stdin "$User" 
+} || {
+	echo "Erro criando usuário $User"
+}
+
+
+}
+
+Remove(){
+
+while getopts ":u:" Opt
+do
+        case $Opt in
+                u)
+                        User="$OPTARG"
+                ;;
+                \?)
+                        echo "Opção inválida: -$OPTARG"
+                        exit 1
+                ;;
+                :)
+                        echo "Opção -$OPTARG necessita argumento."
+                        exit 1
+                ;;
+        esac
+done
+
+while [ ! $User ] 
+do
+        read -p "Informe o usuario: " User
+done
+
+userdel -r $User  && {
+        echo "Usuário $User removido com sucesso"
+} || {  
+        echo "Erro removendo usuário $User"
+} 
+
+}
+
+
+while getopts ":ad" Opt
+do
+	case $Opt in
+		a)
+			Add $@
+		;;
+		d)
+			Remove $@
+		;;
+		*)
+			echo "Use $0 { -a [ -b shell | -h homedir | -p password | -u usuário ] | -d }"
+	esac
+done
