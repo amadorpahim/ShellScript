1fc47a9eef5af9c6b3e89fc4b7e627d1f2185ac4
Amador Pahim <apsegundo@CX71075407.ucs.br>
2011-08-26 17:22:19 -0300

    Alterando sintaxe de atribuição do IFS para uma forma mais didática.

--- parser/parser.sh
+++ parser/parser.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-IFS="
-"
+
+# Inter Field Separator será "enter" --> "0a"
+IFS=$'\x0a'
+
 for LINE in $(cat $config_file | sed 's/#.*//g;s/ = /=/g;/^$/d') 
 do
 	key=$(echo $LINE | cut -d "=" -f 1)
