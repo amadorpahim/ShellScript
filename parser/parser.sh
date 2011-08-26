2e4e816f02b93174d15d89f8abb9fcccd96c29ed
Amador Pahim <apsegundo@CX71075407.ucs.br>
2011-08-26 17:32:25 -0300

    Comentários

--- parser/parser.sh
+++ parser/parser.sh
@@ -3,9 +3,16 @@
 # Inter Field Separator será "enter" --> "0a"
 IFS=$'\x0a'
 
-for LINE in $(cat $config_file | sed 's/#.*//g;s/ = /=/g;/^$/d') 
+# O 'sed' irá:
+## - remover a partir do comentário (#) até o fim da linha;
+## - tirar os espaços antes e depois do '=';
+## - remover as linhas em branco.
+for LINE in $(cat $config_file | sed 's/#.*//g;s/ =/=/g;s/= /=/g;/^$/d') 
 do
+	# pegando a chave da linha
 	key=$(echo $LINE | cut -d "=" -f 1)
+	# pegando o valor da linha
 	value=$(echo $LINE | cut -d "=" -f 2)
+	# criando a variável com mesmo nome da chame e atribuindo o valor
 	eval $key=\"${value}\"
 done
