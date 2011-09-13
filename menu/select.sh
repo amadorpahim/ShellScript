d0075c1b987145dea3feb0055ece80ef651dc2e6
Amador Pahim <amador@pahim.org>
2011-09-13 09:05:04 -0300

    Alguns comentários e remoção do comando em subshell.

--- menu/select.sh
+++ menu/select.sh
@@ -6,15 +6,18 @@ PS3="Digite a opção desejada: "
 # O menu será montado com os itens da lista informada.
 # Cada item do menu receberá um índice antes, que será
 # usado para escolha do item.
-# O item escolhido será atribuído a variável $name e
+# O item escolhido será atribuído a variável $ip e
 # o índice para a variável $REPLY
-select name in $(<lista.txt)
+# É possível criar a lista com comandos executados em um subshell,
+# usando, por exemplo, a sintaxe:
+# select ip in $(cat file.txt)
+select ip in 10.10.0.10 10.10.0.11 10.10.0.12 10.10.0.13
 do
-	# Caso um item inválido seja escolhido, a variável $name
+	# Caso um item inválido seja escolhido, a variável $ip
 	# será nula. Os comandos dentro do select serão executados
 	# a cada seleção, até encontrar um "break".
-	[[ $name ]] && {
-		echo "Você escolheu o IP: $name (opção $REPLY)"
+	[[ $ip ]] && {
+		echo "Você escolheu o IP: $ip (opção $REPLY)"
 		break	
 	} || {
 		echo "Opção $REPLY não existe!"
