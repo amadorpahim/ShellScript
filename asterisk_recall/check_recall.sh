1a0652c5c1bc1bf48aa7d056b8c2eb8c764f7e2b
Amador Pahim <amador@pahim.org>
2011-09-27 16:46:34 -0300

    Inversao de caller/callee

--- asterisk_recall/check_recall.sh
+++ asterisk_recall/check_recall.sh
@@ -1,45 +1,46 @@
 #!/bin/bash 
 sleep 1
 
-RECALL="/root/script/recall.sh"
+RECALL="/usr/local/script/recall.sh"
 AST="/usr/sbin/asterisk"
 LOG_FACILITY="local5.warning"
 
 [ $# -ne 2 ] && {
-	logger -p $LOG_FACILITY -t "$0" "ERRO: Você precisa informar os dois numeros envolvidos no hangup."
-	exit 1
+        logger -p $LOG_FACILITY -t "$0" "ERRO: Você precisa informar os dois numeros envolvidos no hangup."
+        exit 1
 }
 
+
 callee1=$1
-caller1=$($AST -rx "database get recall $callee1")
+caller1_full=$($AST -rx "database show recall" | grep -m 1 ": ${callee1}" | tr -s " " | cut -d " " -f 1)
+caller1=${caller1_full#*_}
 
 callee2=$2
-caller2=$($AST -rx "database get recall $callee2")
+caller2_full=$($AST -rx "database show recall" | grep -m 1 ": ${callee2}" | tr -s " " | cut -d " " -f 1)
+caller2=${caller2_full#*_}
 
-#### TESTA SE ESTÁ OCUPADO #########
+#### TESTAR SE ESTA OCUPADO
 #caller_busy=$($AST -rx "core show channels concise" | cut -d "!" -f 3 | fgrep "${caller}" > /dev/null 2>&1 && echo 0 || echo 1 )
 #
 #[ "${caller_busy}" -eq 0 ] && {
-#	echo nao posso ligar agora
+#       echo nao posso ligar agora
 #} || {
-#	echo ligue agora
+#       echo ligue agora
 #}
 #####################################
 
-[ "${caller1}" ==  "Database entry not found." ] && {
-	logger -p $LOG_FACILITY -t "$0" "Sem espera para $callee1"
-} || {
-	logger -p $LOG_FACILITY -t "$0" "${caller1#* } quer falar com $callee1"
-	$RECALL ${caller1#* } $callee1
-	$AST -rx "database del recall $callee1"
+[ -z "${caller1}" ] && {
+        logger -p $LOG_FACILITY -t "$0" "Sem espera para $callee1"
+} || {  
+        logger -p $LOG_FACILITY -t "$0" "$caller1 quer falar com $callee1"
+        $RECALL $caller1 $callee1
+        $AST -rx "database del recall ${caller1_full##*/}"
 }
 
-[ "${caller2}" ==  "Database entry not found." ] && {
+[ -z "${caller2}" ] && {
         logger -p $LOG_FACILITY -t "$0" "Sem espera para $callee2"
-} || {
-	logger -p $LOG_FACILITY -t "$0" "${caller2#* } quer falar com $callee2"
-	$RECALL ${caller2#* } $callee2
-	$AST -rx "database del recall $callee2"
+} || {  
+        logger -p $LOG_FACILITY -t "$0" "$caller2 quer falar com $callee2"
+        $RECALL $caller2 $callee2
+        $AST -rx "database del recall ${caller2_full##*/}"
 }
-
-
